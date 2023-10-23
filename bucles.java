a9374b10679ce6bc11a9bff3a4e3d75720534fae
Obrayan <117853806+cora21@users.noreply.github.com>
2023-10-23 11:46:30 -0400

    Update bucles.java
    Ejercicio propuesto para practicar y entender los bucles while

--- bucles.java
+++ bucles.java
@@ -2,12 +2,12 @@ import javax.swing.*;
 public class bucles {
     public static void main(String[] args) {
     //todo sobre los bucles:
-    //bucles indeterminados while y do while -  no sabes cuantas veces se va a repetir
-    //bucles determinados for - foreach - si sabemos cuantas veces se va a repetir
+    //bucles indeterminados while y do while -  no sabes cuantas veces se va a repetir en el transcurso de la ejecucion del programa
+    //bucles determinados for - foreach - si sabemos cuantas veces se va a repetir normalmente nosotros elejimos o dependiendo del programa
     
-    String clave = "obrayan";
+    String clave = "obra";
     String pass = "";
-
+    // en este caso evaluamos que la clave y contrasena sean iguales para que el programa continue su ejecucion, hasta que no se coloque la contraseña correcta no saldremos del bucle while
     while (clave.equals(pass) == false){
         pass = JOptionPane.showInputDialog("Introduce la contraseña: ");
         if (clave.equals(pass)==false){
